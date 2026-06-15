bae6f966c55384e3e92aa6a5eeb4d2a4d4a48985
Lindsay <lindsay.marion@acsone.eu>
2026-06-15 09:05:01 +0200

    [19.0][MIG] crm_salesperson_planner: Migration to 19.0

--- crm_salesperson_planner/tests/test_crm_salesperson_planner_visit_template.py
+++ crm_salesperson_planner/tests/test_crm_salesperson_planner_visit_template.py
@@ -159,7 +159,7 @@ class TestCrmSalespersonPlannerVisitTemplate(BaseCommon):
             }
         )
         visit_template.create_visits(days=10)
-        visit_0 = fields.first(visit_template.visit_ids)
+        visit_0 = visit_template.visit_ids[:1]
         event_id_0 = visit_0.calendar_event_id
         self.assertEqual(visit_0.date, event_id_0.start_date)
         visit_0.write({"date": fields.Date.today() + timedelta(days=7)})
@@ -185,7 +185,7 @@ class TestCrmSalespersonPlannerVisitTemplate(BaseCommon):
             }
         )
         visit_template.create_visits(days=10)
-        first_visit = fields.first(visit_template.visit_ids)
+        first_visit = visit_template.visit_ids[:1]
         self.assertTrue(first_visit.calendar_event_id)
         with self.assertRaises(exceptions.ValidationError):
             first_visit.unlink()
